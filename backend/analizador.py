dd8e2fda0ef3e2c0942a833d41bbf85bf9dc2eb2
JoshdlC <josue2003@gmail.com>
2024-10-27 17:00:02 -0600

    clases python

---
+++ backend/analizador.py
@@ -0,0 +1,7 @@
+from diccionarioSent import DiccionarioSent
+from empresa import Empresa, Servicio
+
+
+class Analizador():
+    def __init__(self) -> None:
+        pass
---
+++ backend/diccionarioSent.py
@@ -0,0 +1,6 @@
+class DiccionarioSent():
+    def __init__(self, positivos, negativos):
+        self.positivos = positivos
+        self.negativos = negativos
+        
+        
